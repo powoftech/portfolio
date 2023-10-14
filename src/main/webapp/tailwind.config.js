2b3ba9e07d2c0b032681e872346360ff629c53ce
powoftech <powoftech@gmail.com>
2023-10-15 05:01:38 +0700

    Update homepage

--- src/main/webapp/tailwind.config.js
+++ src/main/webapp/tailwind.config.js
@@ -1,22 +1,22 @@
 // /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      colors: {
-        body: "#070707",
-        "selected-text": "#A3A3FF",
-        theme: "#3F3FFF",
-        nav: "#404053",
-        secondary: "#9191A4",
-        badge: "#3F3F51",
-        "input-border": "#565666",
-        input: "#2A2A35",
-      },
-      fontFamily: {
-        poppins: ["'Poppins'", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			colors: {
+			body: "#17171f",
+				"selected-text": "#A3A3FF",
+				theme: "#3F3FFF",
+				nav: "#404053",
+				secondary: "#9191A4",
+				badge: "#3F3F51",
+				"input-border": "#565666",
+				input: "#2A2A35",
+			},
+			fontFamily: {
+				poppins: ["'Poppins'", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
